8cbfd39b1177944ae40c4c7b6fad8e06423237da
DasZeitreisende <87364398+DasZeitreisende@users.noreply.github.com>
2021-07-13 11:07:12 +0200

    Add files via upload

---
+++ gmod-roleplayjobsystem-master/lua/rpjs/sh_config.lua
@@ -0,0 +1,21 @@
+rpjs.version = "1.9"
+
+rpjs.config = {}
+
+rpjs.config.lang = {}
+rpjs.config.windowtitle = 'JOBS'
+rpjs.config.windowbottomcolor = Color( 45, 62, 80, 255 )
+rpjs.config.windowtopcolor = Color( 50, 62, 80, 255 )
+rpjs.config.bgcolor = Color( 50, 62, 80, 255 )
+rpjs.config.titlecolor = Color( 208, 209, 214, 150 )
+rpjs.config.resumecolor = Color(231,76,60,255)
+rpjs.config.closebtncolor = Color(255,98,0,100)
+rpjs.config.barscolor = Color(255,98,0,255)
+rpjs.config.admins = {
+	"superadmin",
+	"admin"
+}
+rpjs.config.distance = 90  -- DISTANCE TO CHECK WHEN CHANGING JOB AND OPENING MENU
+
+-- SET THIS TO TRU IF YOU WANT TO ACTIVATE DEBUGGING COMMAND: rpjs_debug
+rpjs.config.debug = true
